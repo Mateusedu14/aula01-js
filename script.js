4a2cb5fa18840bedde737be3a5d9c359f7649de4
mateusedu14 <mateus.elazevedo@senacsp.edu.br>
2024-08-20 21:47:20 -0300

    feat: Calculando a média de Aluno em Javascript

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//Exibir no terminal
 console.log("Hello World");
+//Exibir no navegador
+document.write("Olá Navegador");
+//um aler
+alert("Alerta de Confirmação");
 
-document.write("Olá Navegador")
+// Capturar o nome do Aluno
+var nome = window.prompt("Informe o Nome:");
+// Guardar a primeira nota
+var nota1 = window.prompt("Digite a nota1:");
+//converte a nota1 de texto para decimal
+nota1 = parseFloat(nota1)
+    // Guardar a segunda nota
+    var nota2 = window.prompt("Digite a nota2:");
+    // Converte a nota2 de texto para decimal
+    nota2 = parseFloat(nota2)
+//calculo a média das duas notas
+
+var media = (nota1 + nota2)/2;
+//Gero a mensagem de exibição do nome e da média do aluno
+var mensagem = nome + ", você obteve a média : " + media;
+
+window.alert(mensagem);
